4bebdaacb01162985808c2219d721fbc2797855a
alejandro serra blanca <alejandro.serra@billymob.com>
2018-10-20 16:44:32 +0200

    server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 
 
+//const server = require('./server.js');
+//server.start();
+
+
 var exports = module.exports = {
 	start: function() {
 		
-		app.get('/hello', function (req, res) {
+	app.get('/hello', function (req, res) {
 	  res.send('Hello World!');
 	});
 
