b4ad342f202fdc29575561153e82a69b6a1a2e50
Irene <irene@alejandros-MacBook-Air.local>
2018-10-20 16:28:54 +0200

    try_barra

--- src/sensors.js
+++ src/sensors.js
@@ -16,91 +16,44 @@ var X = [0, 0, 255];  // Red
 var O = [255, 0, 0];  // White
 
 var questionMark = [
-O, O, O, X, X, O, O, O,
-O, O, X, O, O, X, O, O,
-O, O, O, O, O, X, O, O,
-O, O, O, O, X, O, O, O,
-O, O, O, X, O, O, O, O,
-O, O, O, X, O, O, O, O,
 O, O, O, O, O, O, O, O,
-O, O, O, X, O, O, O, O
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
 ];
 
 senseLeds.setPixels(questionMark);
 
-senseLeds.getPixels((err, pixelArray)=>{
-var arrayNueva = _.clone(pixelArray);
-arrayNueva.splice(56,8);
-const newLine = [0, 0, 0, 0, 0, 0, 0, 0];
+setInterval(() => {
 
-const lastRow = newLine.map((elem,i)=>{
-	if(i < positionBar || i >= positionBar + BAR_LENGTH){
-		return O
-	}
-	return v;
-});
+	senseLeds.getPixels((err, pixelArray)=>{
+
+	var arrayNueva = _.clone(pixelArray);
+	arrayNueva.splice(56,8);
+	const newLine = [0, 0, 0, 0, 0, 0, 0, 0];
+
+	const lastRow = newLine.map((elem,i)=>{
+		if(i < positionBar || i >= positionBar + BAR_LENGTH){
+			return O
+		}
+		return v;
+	});
 
 
-senseLeds.setPixels([...arrayNueva,...lastRow]);
+	senseLeds.setPixels([...arrayNueva,...lastRow]);
 
-});
+	});
 
-const newLine = [0,0,0,0,0,0,0,0];
+	positionBar++;
 
+},2000);
 
 
 /*
-
-// const pressCallback = (direction) = {
-//
-//   	// const sensorData = null;
-//
-//   	IMU.getValue((err, data) => {
-//   		if (err !== null) {
-// 		    console.error("Could not read sensor data: ", err);
-// 		    return;
-// 			}
-// 		// sensorData = data;
-//   	});
-//
-//   	switch(direction) {
-//   		case "up":
-//   		 	console.log("Temp is: ", sensorData.temperature);
-//   		 	break;
-//   		 case "down":
-//   		 	console.log("Humidity is: ", sensorData.humidity);
-//   		 	break;
-//   	}
-// }
-
-const holdCallback = (direction) => {
-
-  	const sensorData = null;
-
-  	IMU.getValue((err, data) => {
-  	// 	if (err !== null) {
-		 //    console.error("Could not read sensor data: ", err);
-		 //    return;
-    // }
-		 //  sensorData = data;
-  	// });
-
-	  	switch(direction) {
-	  		case "up":
-	  			console.log("Gyroscope is: ", JSON.stringify(data.gyro, null, "  "));
-	  		 	break;
-	  		case "down":
-	  			console.log("Temp is: ", data.temperature);
-	  			break;
-	  	}
-  	});
-
-}
-
-JoystickLib.getJoystick().then(joystick => {
-  //joystick.on("press", pressCallback);
-  joystick.on("hold", holdCallback);
-});
 
 setInterval(() => {
 
